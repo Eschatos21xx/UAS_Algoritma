2cb5abc51dd3159572920a4d5e1a7494ef4f05b7
jndan21 <118275197+Eschatos21xx@users.noreply.github.com>
2026-01-19 02:41:03 +0000

    CommitAseli

--- UAS2.java
+++ UAS2.java
@@ -1,12 +1,11 @@
-public class Pattern3 {
+public class UAS2 {
         public static void main(String[] args) {
-                for (int i = 5; i >= 1; i--) {
-                            for (int j = 1; j <= i; j++) {
-                                            System.out.print("* ");
+                for (int i = 1; i <= 4; i++) {
+                            for (int j = 1; j <= 4; j++) {
+                                            System.out.print(i + " ");
                                                         }
                                                                     System.out.println();
                                                                             }
                                                                                 }
                                                                                 }
-                                                                                
-}
+
